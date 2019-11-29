3500bb6e6f73a889aba51797d423034e63d5a88b
Suizhe.Li <suizhe.li@outlook.com>
2019-11-29 10:39:14 +0900

    server

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net"
 	"sync"
 
 	pb "github.com/lisuizhe/microservices-in-golang/proto/consignment"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 const (
@@ -18,4 +23,45 @@ type repository interface {
 type Repository struct {
 	mu           sync.RWMutex
 	consignments []*pb.Consignment
+}
+
+// Create ...
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	repo.mu.Lock()
+	updated := append(repo.consignments, consignment)
+	repo.consignments = updated
+	repo.mu.Unlock()
+	return consignment, nil
+}
+
+type service struct {
+	repo repository
+}
+
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+func main() {
+	repo := &Repository{}
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+
+	pb.RegisterShippingServiceServer(s, &service{repo})
+
+	reflection.Register(s)
+
+	log.Println("Running on port:", port)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
